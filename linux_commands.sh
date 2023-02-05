619a9fa303403227bf2ab0acc4464e042a6055b8
Walt <wangchonghuan@gmail.com>
2023-02-05 01:47:39 +0000

    Update linux_commands.sh

--- linux_commands.sh
+++ linux_commands.sh
@@ -21,8 +21,15 @@ curl -fsSL https://get.docker.com | bash -s docker
 docker run --rm -p 3000:3000 simple-node
 docker ps
 docker stop {CONTAINER_ID}
+
+# kuard
+git clone https://github.com/kubernetes-up-and-running/kuard
+cd kuard
+docker build -t kuard .
+docker run --rm -p 8080:8080 kuard
 docker tag kuard gcr.io/kuar-demo/kuard-amd64:blue
-docker push gcr.io/kuar-demo/kuard-amd64:blue
+docker push gcr.io/kuar-demo/kuard-amd64:blue #Unauthorized access.
+docker run -d --name kuard --publish 8080:8080 gcr.io/kuar-demo/kuard-amd64:blue
 
 #golang
 apt-cache policy golang
